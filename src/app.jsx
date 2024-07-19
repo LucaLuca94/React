34bbe517aa0a377dac127f3e20c60f9cc2d957eb
Luca Potenza <potenzaluca32@gmail.com>
2024-07-19 11:39:37 +0200

    added exercise

---
+++ src/app.jsx
@@ -0,0 +1,10 @@
+import { HelloWorld, Message, MessageText } from "./helloWord";
+
+export function App() {
+  return (
+    <div>
+      <HelloWorld />
+      <MessageText />
+    </div>
+  );
+}
---
+++ src/helloWord.jsx
@@ -0,0 +1,6 @@
+export function HelloWorld() {
+  return <h2>Hello World!</h2>;
+}
+export function MessageText() {
+  return <h3>What a beautiful day!</h3>
+}
