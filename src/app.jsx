6a8345d2ee39a619f914e294aecdbecf0ce2ccfa
Luca Potenza <potenzaluca32@gmail.com>
2024-07-23 17:09:43 +0200

    added time exercise

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,14 @@
-import { Age, Welcome } from "./welcome";
+import { AllertClock } from "./allertClock";
 
 export function App() {
-  return (
-    <div>
-      
-      <Welcome name="Luca"  />
-      <Age age={29} />
-    </div>
-  );
-}
+  
+    return (
+      <div>
+        <AllertClock onClick={handleButtonClick} />
+
+        {/* <Welcome name="Luca" />
+        <Age age={29} /> */}
+      </div>
+    );
+  }
+
--- src/welcome.jsx
+++ src/welcome.jsx
@@ -1,13 +1,17 @@
-export function Welcome({ name}) {
+import { Message } from "./message";
+
+export function Welcome({ name, age }) {
   return (
     <div>
-      <p>
-        Welcome, <b>{name}</b>
-      </p>
-      
+      <p>Welcome, {name}</p>
+      {age > 18 && <p>You are {age} Years old</p>}
+      {age && <p>You are {age} Years old</p>}
+      {age > 18 && age < 65 ? <p>You are {age} years old</p> : null}
+      {age > 18 && name === "John" ? (
+        <p>
+          You are {age} years old, {name}
+        </p>
+      ) : null}
     </div>
   );
-}
-export function Age ({age}) {
-  return <p>You are {age} years old</p>;
 }
